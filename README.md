8a200edf6bbab90abf2ba0ea4f59365b7dd43155
Ashley Coles <ashley@acoles.co.uk>
2020-06-22 10:36:32 +0100

    Adding basic typescript setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # gulp-setup
 
-Example gulp setup with SCSS compilation and Babel transpilation.
+Example gulp setup with SCSS TypeScript.
 
 Configured to watch for changes in `app/js` and `app/scss`
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
 
-function babelCompile(cb) {
-    return gulp.src("app/js/*.js")
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("app/js/dist"));
+function tsCompile(cb) {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("app/js/dist"));
     cb();
 }
 
@@ -23,9 +22,9 @@ function sassCompile(cb) {
 
 function watch() {
     gulp.watch("app/scss/**/*.scss", sassCompile);
-    gulp.watch("app/js/*.js", babelCompile);
+    gulp.watch("app/js/*.ts", tsCompile);
 }
 
 exports.sass = sassCompile;
-exports.babel = babelCompile;
+exports.ts = tsCompile;
 exports.watch = watch;
