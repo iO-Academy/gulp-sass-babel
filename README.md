8f720929b7cdb5b75f850753784bcf4e1b9f242a
Ashley Coles <ashley@acoles.co.uk>
2020-06-22 10:33:47 +0100

    Basic gulp setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gulp-setup
+
+Example gulp setup with SCSS compilation and Babel transpilation.
+
+Configured to watch for changes in `app/js` and `app/scss`
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+
+function babelCompile(cb) {
+    return gulp.src("app/js/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("app/js/dist"));
+    cb();
+}
+
+function sassCompile(cb) {
+    return gulp.src("app/scss/style.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("app/css"));
+    cb();
+}
+
+function watch() {
+    gulp.watch("app/scss/**/*.scss", sassCompile);
+    gulp.watch("app/js/*.js", babelCompile);
+}
+
+exports.sass = sassCompile;
+exports.babel = babelCompile;
+exports.watch = watch;
